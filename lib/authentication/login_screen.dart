a1b6a2ffcc073a1571de228df858daba66865ce2
Arsene <arskos.dev@gmail.com>
2024-01-13 15:05:51 +0000

    Login screen ongoing

--- lib/authentication/login_screen.dart
+++ lib/authentication/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rsf/widgets/input_text_widget.dart';
 
 
 class LoginScreen extends StatefulWidget {
@@ -12,6 +13,11 @@ class LoginScreen extends StatefulWidget {
 
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+  bool showProgressBar = false;
+
   @override
   Widget build(BuildContext context) {
 
@@ -26,7 +32,7 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
 
               Image.asset(
-              'assets/images/White-Logo.png',
+              'assets/images/RSF.png',
               width: 200,
             ),
 
@@ -49,12 +55,122 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
 
               const SizedBox(
-                height: 100,
+                height: 30,
               ),
 
               //email input
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+
+                child: InputTextWidget(
+                  textEditingController: emailTextEditingController,
+                  labelString: "Email",
+                  iconData: Icons.email_outlined,
+                  isObscure: false,
+                ),
+              ),
+
+              const SizedBox(
+                height: 30,
+              ),
 
               //password input
+
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+
+                child: InputTextWidget(
+                  textEditingController: passwordTextEditingController,
+                  labelString: "Password",
+                  iconData: Icons.lock_outlined,
+                  isObscure: true,
+                ),
+              ),
+
+              const SizedBox(
+                height: 30,
+              ),
+
+              //Login button
+
+              //no account yet
+              showProgressBar == false ?
+              Column(
+                children: [
+                  //login button
+                  Container(
+                    width: MediaQuery.of(context).size.width - 38,
+                    height: 54,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: InkWell(
+                      onTap: (){
+                        setState(() {
+                          showProgressBar = true;
+                        });
+
+                        //loading...
+
+                      },
+                      child: const Center(
+                        child: Text(
+                        "Login",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700,
+
+                        )
+                      ),
+
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(
+                    height: 30,
+                  ),
+
+                  //no account yet, signup now
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Don't have an account ? ",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: (){
+                          //send user to signup screen
+
+
+                        },
+                        child: const Text(
+                          "Signup now",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          )
+                        ),
+                      )
+                    ],
+
+                  ),
+
+                ],
+              ): Container(
+              //show animations
+              ),
 
 
             ],
